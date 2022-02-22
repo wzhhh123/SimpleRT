7161a1fb9d48e7760fff221fe02f6e9e3dbc6afd
wzhhh123 <421745720@qq.com>
2022-02-22 09:45:20 +0800

    add texture

--- src/base/camera.h
+++ src/base/camera.h
@@ -1,13 +1,15 @@
 #pragma once
 
 #include "header.h"
+#include "ray.h"
 
 class Camera
 {
 public:
 	Camera();
 	void GenerateRay(int Idx, dVec2 Sample, Ray& R);
-
+    void GenerateRayDifferential(int Idx, dVec2& Samples, RayDifferential *Rd);
+    
 private:
 	dVec3 dxCamera, dyCamera;
 	float dirZ;
